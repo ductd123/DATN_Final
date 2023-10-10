0bf3990d08521cae14ed90011a130bdb6af2899c
anhtucao <caoanhtu611@gmail.com>
2023-10-11 00:00:40 +0700

    tạo layout cho page

--- scproject/src/Component/HomePage.jsx
+++ scproject/src/Component/HomePage.jsx
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
 
+import HomepageLeft from './HomepageLeft'
+import HomepageMid from './HomepageMid'
+import HomepageRight from './HomepageRight'
+import "./homepage.css"
 export default class HomePage extends Component {
   render() {
     return (
-      <div>HomePage</div>
+      <div className='flex'>
+        <HomepageLeft/>
+        <HomepageMid/>
+        <HomepageRight/>
+      </div>
     )
   }
 }
